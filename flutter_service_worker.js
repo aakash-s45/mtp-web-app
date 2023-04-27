839af3f04e1674d65a9af54c8708b42fd9068fd4
Aakash Solanki <tech2aks@gmail.com>
2023-04-27 15:21:40 +0530

    Fix gestures

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "48438d813ee13dd924ea93336e679ed9",
-"index.html": "48dff0bc7836c6cde1da04419ebefa51",
-"/": "48dff0bc7836c6cde1da04419ebefa51",
-"main.dart.js": "521558b43f0a8c3a53ebe19a8e087d3f",
+"index.html": "59952f75c91d7b4cf4aa367b435cee63",
+"/": "59952f75c91d7b4cf4aa367b435cee63",
+"main.dart.js": "3a14ff8e796ad44465ee89400113f493",
+"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -14,11 +15,16 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "e2a08e0c5d3748077e1c0919bd84ca69",
 "assets/asset/mountains.png": "02f50636f777cfc678d0fd66039b52e4",
-"assets/AssetManifest.json": "10dcdfa5b6da2e80539975b0fb883ca6",
+"assets/asset/compass.png": "e55c0965b52421a78c66e6049bb2fe10",
+"assets/AssetManifest.json": "fadfc83ca1dfaf10fe73d20c743590cd",
 "assets/NOTICES": "f88de8f1e1d49cd7650fdfef8521b184",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080"
+"assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
+"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
 };
 
 // The application shell files that are downloaded before a service worker can
